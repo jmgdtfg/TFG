aad24c6e4135855cc320fd29125496c8b4b509a2
jmgdtfg <34899529+jmgdtfg@users.noreply.github.com>
2018-02-18 17:14:08 +0100

    Add files via upload

---
+++ APIS/src/openweathermap/OpenWeatherMapManager.java
@@ -0,0 +1,82 @@
+package openweathermap;
+
+import java.util.List;
+import net.aksingh.owmjapis.api.APIException;
+import net.aksingh.owmjapis.core.OWM;
+import net.aksingh.owmjapis.model.CurrentWeather;
+import net.aksingh.owmjapis.model.HourlyWeatherForecast;
+import net.aksingh.owmjapis.model.param.WeatherData;
+
+public class OpenWeatherMapManager {
+
+	private String token_ = {TOKEN};
+	private OWM owm_ = new OWM(token_);
+	
+	//Constructor por defecto
+	public OpenWeatherMapManager(){}
+
+	//Constructor parametrizado
+	public OpenWeatherMapManager(String token){
+		token_=token;
+	}
+	
+	//Función que devuelve la temperatura actual de una ciudad pasada por parámetro.
+	public int getCurrentTemp(String city) throws APIException {
+		CurrentWeather cwd = owm_.currentWeatherByCityName(city);
+		int Kelvin = cwd.getMainData().getTemp().intValue();
+		int Centigrados = Kelvin - 273;
+		return Centigrados;
+	}
+	//Función que devuelve la maxima temperatura del día de una ciudad pasada por parámetro.
+	public int getMaxTemp(String city) throws APIException {
+		CurrentWeather cwd = owm_.currentWeatherByCityName(city);
+		int Kelvin = cwd.getMainData().getTempMax().intValue();
+		int Centigrados = Kelvin - 273;
+		return Centigrados;
+	}
+	//Función que devuelve la mínima temperatura del día de una ciudad pasada por parámetro.
+	public int getMinTemp(String city) throws APIException {
+		CurrentWeather cwd = owm_.currentWeatherByCityName(city);
+		int Kelvin = cwd.getMainData().getTempMin().intValue();
+		int Centigrados = Kelvin - 273;
+		return Centigrados;
+	}
+	//Función que devuelve la humidad de una ciudad pasada por parámetro
+	public int getHumidity(String city) throws APIException {
+		CurrentWeather cwd = owm_.currentWeatherByCityName(city);
+		int humidity = cwd.getMainData().getHumidity().intValue();
+		return humidity;
+	}
+	//Función que devuelve la lluvia de una ciudad pasada por parámetro
+	public int getCloudiness(String city) throws APIException {
+		CurrentWeather cwd = owm_.currentWeatherByCityName(city);
+		//Devuelve un porcentaje ej. 93% nubosidad
+		int cloudiness = cwd.getCloudData().getCloudiness();
+		return cloudiness;
+
+	}
+	//Pronóstico
+	public List<WeatherData> HourlyForecast(String city) throws APIException{
+
+		//DailyWeatherForecast;
+		HourlyWeatherForecast hwf = owm_.hourlyWeatherForecastByCityName(city);
+		List<WeatherData> wd = hwf.getDataList();
+
+		/*				for (int i=0;i<wd.size();i++){
+					System.out.println(wd.get(i).getDateTime());
+					System.out.println(wd.get(i).getMainData().getTemp().intValue());
+					System.out.println(wd.get(i).getCloudData().getCloudiness());
+				}*/
+
+		return wd;
+		//System.out.println(wd.get(1).getCloudData().toString());
+	}
+	/*
+	public int getRain(String city) throws APIException {
+		CurrentWeather cwd = owm_.currentWeatherByCityName(city);
+		int rain = cwd.getRainData().getPrecipVol3h();
+		return rain;
+	}*/
+
+
+}
